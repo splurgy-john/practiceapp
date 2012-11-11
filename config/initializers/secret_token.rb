be8efcf31ad6fa7de0df17d238e2431045ed790c
John Katsnelson <john@splurgy.org>
2012-11-11 15:08:09 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practiceapp::Application.config.secret_token = '070f4bd9a2dfde718991a4c6e2dddf12dbb018204068392de5d0031345687eae6ba09057044e951e087bb6f762a9861cdd9b5d6c148aa40a1e4baedfccf350be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Practiceapp::Application.config.session_store :cookie_store, key: '_practiceapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Practiceapp::Application.config.session_store :active_record_store
